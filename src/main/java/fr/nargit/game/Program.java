79fb400f3f0ca36836a03602a08d714cd6a51c48
Tigran Tch <nargitinthenight@gmail.com>
2016-02-01 23:41:14 +0100

    prepare web service

--- src/main/java/fr/nargit/game/Program.java
+++ src/main/java/fr/nargit/game/Program.java
@@ -1,10 +1,7 @@
 package fr.nargit.game;
 
-import fr.nargit.game.domain.Match;
-import fr.nargit.game.domain.Player;
-
-import java.util.Date;
-import java.util.List;
+import fr.nargit.game.ranking.algorihtm.elo.EloAlgorithm;
+import fr.nargit.game.ranking.algorihtm.elo.performance.DefaultEloPerformance;
 
 /**
  * (c) 17-janv.-2016
@@ -14,35 +11,7 @@ import java.util.List;
 public class Program {
 
   public static void main(String[] args) {
-    // Create Players
-    Player michael = new Player("michael");
-    Player tigran = new Player("tigran");
-    Player nico = new Player("nico");
-    Player quentin = new Player("quentin");
+    EloAlgorithm eloAlgorithm = new EloAlgorithm(new DefaultEloPerformance());
 
-    // Create Tournament
-    TournamentManagement tournamentManagement = new TournamentManagement(michael, tigran, nico, quentin);
-
-    // Add matches
-    Date today = new Date();
-    tournamentManagement.createMatch(today, tigran, michael);
-    tournamentManagement.createMatch(today, nico, quentin);
-    tournamentManagement.createMatch(today, michael, quentin);
-    tournamentManagement.createMatch(today, nico, tigran);
-    tournamentManagement.createMatch(today, michael, nico);
-    tournamentManagement.createMatch(today, quentin, tigran);
-
-    showTournament(tournamentManagement);
-  }
-
-  private static void showTournament(TournamentManagement tournamentManagement) {
-    int i = 0;
-    for (Match match : tournamentManagement.getMatches()) {
-      System.out.println(String.format("Match %d: all players %s", i, match.getPlayers()));
-      System.out.println(String.format("Winners : %s", match.getWinners()));
-      System.out.println(String.format("Losers  : %s", match.getLosers()));
-      System.out.println(String.format("Draw    : %s \n", match.getDraws()));
-      i++;
-    }
   }
 }
